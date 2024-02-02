51e9ef5691acf30b5929a919c11c067be8bb3758
tbutani <tbutani@NO-002-QA.nowonline.server>
2024-02-02 19:15:50 +0530

    Update script-HomePage

--- FreeCRMTestOne/src/main/java/com/crm/qa/pages/HomePage.java
+++ FreeCRMTestOne/src/main/java/com/crm/qa/pages/HomePage.java
@@ -9,49 +9,50 @@ import com.crm.qa.base.TestBase;
 public class HomePage extends TestBase{
 	
 	//Page factory or Object Repository
-	@FindBy(xpath="//*[@id=\"main-nav\"]/nav/div/div[2]/a/img")
-	WebElement userNameLabel;
-		
-	@FindBy(xpath="//*[@id=\"main-nav\"]/nav/div/div[1]/a/img[1]")
-	WebElement imageLogo;
-		
-	@FindBy(xpath="//*[@id=\"navbarSupportedContent\"]/ul/li[3]/a")
-	WebElement myAssignmentsLink;
-		
-	@FindBy(xpath="//*[@id=\"navbarSupportedContent\"]/ul/li[1]/a")
-	WebElement dashboardLink;
-		
-	@FindBy(xpath="//*[@id=\"navbarSupportedContent\"]/ul/li[2]/a")
-	WebElement matchTab;
-		
-	@FindBy(xpath="//*[@id=\"dashboard\"]/div[3]/div[2]/div/div[2]/div[2]/div[2]/div[1]/button/span")
-	WebElement acceptAllCookies;
-		
-	//Initialization
-	public HomePage() {
-		PageFactory.initElements(Driver, this);
-	}
-		
-	//Actions
-	public String verifyHomePageTitle() {
-		return Driver.getTitle();
-	}
-		
-	public boolean verifyImageLogo() {
-		return imageLogo.isDisplayed();
-	}
-		
-	public void clickMatchTab() {
-		matchTab.click();
-	}
-		
-	public MyAssignmentsPage clickMyAssignmentsLink() {
-		myAssignmentsLink.click();
-		return new MyAssignmentsPage();
-	}
-		
-	public void clickAcceptAllCookies() {
-		acceptAllCookies.click();
-	}
+		@FindBy(xpath="//*[@id=\"main-nav\"]/nav/div/div[2]/a/img")
+		WebElement userNameLabel;
+			
+		@FindBy(xpath="//*[@id=\"main-nav\"]/nav/div/a/img[1]")
+		WebElement imageLogo;
+			
+		@FindBy(xpath="//a[@href='/mijn-opdrachten']")
+		WebElement myAssignmentsLink;
+			
+		@FindBy(xpath="//*[@id=\"navbarSupportedContent\"]/ul/li[1]/a")
+		WebElement dashboardLink;
+			
+		@FindBy(xpath="//*[@id=\"navbarSupportedContent\"]/ul/li[2]/a")
+		WebElement matchLink;
+			
+		@FindBy(xpath="/html/body/div[3]/div[2]/div/div[2]/div[2]/div[2]/div[1]/button/span")
+		WebElement acceptAllCookies;
+			
+		//Initialization
+		public HomePage() {
+			PageFactory.initElements(Driver, this);
+		}
+			
+		//Actions
+		public String verifyHomePageTitle() {
+			return Driver.getTitle();
+		}
+			
+		public boolean verifyImageLogo() {
+			return imageLogo.isDisplayed();
+		}
+			
+		public HomePageFree clickMatchLink() {
+			matchLink.click();
+			return new HomePageFree();
+		}
+			
+		public MyAssignmentsPage clickMyAssignmentsLink() {
+			myAssignmentsLink.click();
+			return new MyAssignmentsPage();
+		}
+			
+		public void clickAcceptAllCookies() {
+			acceptAllCookies.click();
+		}
 
 }

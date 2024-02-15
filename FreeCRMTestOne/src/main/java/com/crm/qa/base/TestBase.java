86262fe0d3e4f8de173aa821de796e6e936303ed
tbutani <tbutani@NO-002-QA.nowonline.server>
2024-02-15 16:39:55 +0530

    Update script-TestBase

--- FreeCRMTestOne/src/main/java/com/crm/qa/base/TestBase.java
+++ FreeCRMTestOne/src/main/java/com/crm/qa/base/TestBase.java
@@ -21,8 +21,7 @@ public class TestBase {
 	public TestBase() {
 		try {
 		    prop=new Properties();
-		    FileInputStream ip=new FileInputStream("C:\\Users\\tbutani\\git\\FreeCRMTestOne\\FreeCRMTestOne\\src\\"
-		    		+ "main\\java\\com\\crm\\qa\\config\\config.properties");
+		    FileInputStream ip=new FileInputStream("C:\\chromedriver\\Git\\config.properties");
 		    prop.load(ip);
 		    
 		}catch(FileNotFoundException e) {

d5e32f4df2076c65b8ebd6b677056272307fffd6
tbutani <tbutani@NO-002-QA.nowonline.server>
2024-02-15 15:27:40 +0530

    Update script-AddCommandPageTest

--- FreeCRMTestOne/src/test/java/com/crm/qa/testcases/AddCommandPageTest.java
+++ FreeCRMTestOne/src/test/java/com/crm/qa/testcases/AddCommandPageTest.java
@@ -43,7 +43,6 @@ public class AddCommandPageTest extends TestBase{
 		
 	}
 	
-	@SuppressWarnings("static-access")
 	@Test
 	public void uploadFileTest()throws Exception {
 		addCommandPage.clickChooseFile();

c74147b6115c8e272c3e17726213e3c2005a2216
tbutani <tbutani@NO-002-QA.nowonline.server>
2024-03-04 15:43:16 +0530

    UlogoMyreactionsPage

--- FreeCRMTestOne/src/main/java/com/crm/qa/pages/UlogoMyreactionsPage.java
+++ FreeCRMTestOne/src/main/java/com/crm/qa/pages/UlogoMyreactionsPage.java
@@ -8,52 +8,52 @@ import com.crm.qa.base.TestBase;
 
 public class UlogoMyreactionsPage extends TestBase{
 	//Page factory OR Object Repository
-			@FindBy(xpath="//input[contains(@id,'status')]")
-			WebElement clickStatusDropDown;
-			
-			@FindBy(xpath="//li[contains(@id, 'status-1')]")
-			WebElement selectStatusNew;
-			
-			@FindBy(xpath="//li[contains(@id, 'status-2')]")
-			WebElement selectStatusIntreatment;
-			
-			@FindBy(xpath="//li[contains(@id, 'status-3')]")
-			WebElement selectStatusOninterview;
-			
-			@FindBy(xpath="//li[contains(@id, 'status-4')]")
-			WebElement selectStatusRegisteragreement;
-			
-			@FindBy(xpath="//li[contains(@id, 'status-5')]")
-			WebElement selectStatusWithdrawnRejected;
-			
-			//Initialization
-			public UlogoMyreactionsPage() {
-				PageFactory.initElements(Driver, this);
-			}
-			
-			//Actions
-			public void clickStatusDropDown() {
-				clickStatusDropDown.click();
-			}
-			
-			public void selectStatusNew() {
-				selectStatusNew.click();
-			}
-			
-			public void selectStatusIntreatment() {
-				selectStatusIntreatment.click();
-			}
-			
-			public void selectStatusOninterview() {
-				selectStatusOninterview.click();
-			}
-			
-			public void selectStatusRegisterAgreement() {
-				selectStatusRegisteragreement.click();
-			}
-			
-			public void selectStatusWithdrawnRejected() {
-				selectStatusWithdrawnRejected.click();
-			}
+	@FindBy(xpath="//input[contains(@id,'status')]")
+	WebElement clickStatusDropDown;
+	
+	@FindBy(xpath="//li[contains(@id, 'status-1')]")
+	WebElement selectStatusNew;
+	
+	@FindBy(xpath="//li[contains(@id, 'status-2')]")
+	WebElement selectStatusIntreatment;
+	
+	@FindBy(xpath="//li[contains(@id, 'status-3')]")
+	WebElement selectStatusOninterview;
+	
+	@FindBy(xpath="//li[contains(@id, 'status-4')]")
+	WebElement selectStatusRegisteragreement;
+	
+	@FindBy(xpath="//li[contains(@id, 'status-5')]")
+	WebElement selectStatusWithdrawnRejected;
+	
+	//Initialization
+	public UlogoMyreactionsPage() {
+		PageFactory.initElements(Driver, this);
+	}
+	
+	//Actions
+	public void clickStatusDropDown() {
+		clickStatusDropDown.click();
+	}
+	
+	public void selectStatusNew() {
+		selectStatusNew.click();
+	}
+	
+	public void selectStatusIntreatment() {
+		selectStatusIntreatment.click();
+	}
+	
+	public void selectStatusOninterview() {
+		selectStatusOninterview.click();
+	}
+	
+	public void selectStatusRegisterAgreement() {
+		selectStatusRegisteragreement.click();
+	}
+	
+	public void selectStatusWithdrawnRejected() {
+		selectStatusWithdrawnRejected.click();
+	}
 
 }

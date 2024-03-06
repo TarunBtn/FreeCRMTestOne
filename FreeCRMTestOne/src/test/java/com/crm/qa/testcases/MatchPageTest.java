cdb0fc107624d920a420263512495d055e99b559
tbutani <tbutani@NO-002-QA.nowonline.server>
2024-03-06 20:04:01 +0530

    MatchPageTest

--- FreeCRMTestOne/src/test/java/com/crm/qa/testcases/MatchPageTest.java
+++ FreeCRMTestOne/src/test/java/com/crm/qa/testcases/MatchPageTest.java
@@ -1,6 +1,7 @@
 package com.crm.qa.testcases;
 
 import org.openqa.selenium.ElementClickInterceptedException;
+import org.openqa.selenium.NoSuchElementException;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -31,7 +32,11 @@ public class MatchPageTest extends TestBase{
 		testUtil.testWaitFourteen();
 		homePage.clickAcceptAllCookies();
 		testUtil.testWaitEleven();
-		homePage.clickMatchLink();
+		try {
+		    homePage.clickMatchLink();
+		}catch(NoSuchElementException e) {
+			e.printStackTrace();
+		}
 		testUtil.testWaitEight();
 				
 	}

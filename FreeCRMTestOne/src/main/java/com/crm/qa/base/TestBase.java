13bc15d253b6d306293a3796b1a720706e0ed781
tbutani <tbutani@NO-002-QA.nowonline.server>
2024-06-26 16:40:07 +0530

    Testbase

--- FreeCRMTestOne/src/main/java/com/crm/qa/base/TestBase.java
+++ FreeCRMTestOne/src/main/java/com/crm/qa/base/TestBase.java
@@ -41,7 +41,7 @@ public class TestBase {
 		String browserName=prop.getProperty("browser");
 		if(browserName.equals("chrome")) {
 			//System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
-			 String chromeDriverPath = System.getenv("chromium-browser") != null ? System.getenv("chromium-browser") : "/usr/bin/chromium-browser";
+			 String chromeDriverPath = System.getenv("chromium") != null ? System.getenv("chromium") : "/usr/bin/chromium-browser";
 	         System.setProperty("webdriver.chrome.driver", chromeDriverPath);
 			
 			ChromeOptions options = new ChromeOptions();
